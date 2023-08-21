01d7037a5527353e133a75b9f58a189d8948ebed
Igor Sirotin <sir.dalbag@gmail.com>
2023-08-21 19:09:19 +0600

    update

--- less16/main.py
+++ less16/main.py
@@ -1,3 +1,12 @@
 # Task 1. Анаграмм.
 def anagram(s1: str, s2: str) -> bool:
     return sorted(s1) == sorted(s2)
+
+
+def is_anagram(s1: str, s2: str) -> bool:
+    if len(s1) == 0:
+        return True
+    if s1[0] in s2:
+        return is_anagram(s1[1:], s2)
+    else:
+        return False
