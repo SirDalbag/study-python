73db10d51b92815d2adbf29157a53d705e7b1213
Igor Sirotin <sir.dalbag@gmail.com>
2023-08-21 19:13:42 +0600

    update

--- less16/main.py
+++ less16/main.py
@@ -4,9 +4,9 @@ def anagram(s1: str, s2: str) -> bool:
 
 
 def is_anagram(s1: str, s2: str) -> bool:
-    if len(s1) == 0:
+    if len(s1) == 0 and len(s2) == 0:
         return True
     if s1[0] in s2:
-        return is_anagram(s1[1:], s2)
+        return is_anagram(s1[1:], s2.replace(s1[0], "", 1))
     else:
         return False
