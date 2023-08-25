690e3253a8a5d12990ab5b12492daac01ca1921e
Igor Sirotin <sir.dalbag@gmail.com>
2023-08-25 19:35:26 +0600

    update

--- less18/main.py
+++ less18/main.py
@@ -1,1 +1,18 @@
+import openpyxl
+from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
 
+
+def sum(path):
+    workbook: Workbook = openpyxl.load_workbook(path)
+    worksheet: Worksheet = workbook.active
+    sum = 0
+    for row in worksheet.iter_rows():
+        for cell in row:
+            if isinstance(cell.value, (int, float)):
+                sum += cell.value
+    return sum
+
+
+if __name__ == "__main__":
+    print(sum("less18/Лист.xlsx"))
