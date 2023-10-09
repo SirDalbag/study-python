e509374323c27c6ddabd9ad30502e6e09dcb8f78
SirDalbag <sir.dalbag@gmail.com>
2023-10-09 11:19:38 +0600

    update

---
+++ less23/main.py
@@ -0,0 +1,149 @@
+class Solution:
+    @staticmethod
+    def is_power_of_two(n: int) -> bool:
+        if n < 1:
+            return False
+        elif n == 1:
+            return True
+        return Solution.is_power_of_two(n / 2)
+
+    @staticmethod
+    def hamming_weight(n: int) -> int:
+        return bin(n).count("1")
+
+    @staticmethod
+    def my_sqrt(x: int) -> int:
+        l, r = 1, x
+        while l <= r:
+            mid = (l + r) // 2
+            sq = mid * mid
+            if sq == x:
+                return mid
+            elif sq < x:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l - 1
+
+    @staticmethod
+    def add_binary(a: str, b: str) -> str:
+        return str(bin(int(a, 2) + int(b, 2)))[2:]
+
+    @staticmethod
+    def fizz_buzz(n: int) -> list[str]:
+        arr = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                arr.append("FizzBuzz")
+            elif i % 3 == 0:
+                arr.append("Fizz")
+            elif i % 5 == 0:
+                arr.append("Buzz")
+            else:
+                arr.append(str(i))
+        return arr
+
+    @staticmethod
+    def num_unique_emails(emails: list[str]) -> int:
+        unique = set()
+        for i in emails:
+            name, domain = i.split("@")
+            name = name.split("+")[0].replace(".", "")
+            unique.add(f"{name}@{domain}")
+        return len(unique)
+
+    @staticmethod
+    def num_jewels_in_stones(jewels: str, stones: str) -> int:
+        count = 0
+        for i in stones:
+            if i in jewels:
+                count += 1
+        return count
+
+    @staticmethod
+    def first_uniq_char(s: str) -> int:
+        for i, c in enumerate(s):
+            if s.count(c) == 1:
+                return i
+        return -1
+
+    @staticmethod
+    def find_disappeared_numbers(nums: list[int]) -> list[int]:
+        return list(set(range(1, len(nums) + 1)) - set(nums))
+
+    @staticmethod
+    def count_bits(n: int) -> list[int]:
+        return [bin(x).count("1") for x in range(n + 1)]
+
+    @staticmethod
+    def hamming_distance(x: int, y: int) -> int:
+        res = 0
+        while x or y:
+            res += (x & 1) != (y & 1)
+            x >>= 1
+            y >>= 1
+        return res
+
+    @staticmethod
+    def find_content_children(g: list[int], s: list[int]) -> int:
+        g.sort()
+        s.sort()
+        res = 0
+        index = 0
+        for i in range(len(g)):
+            while index < len(s) and g[i] > s[index]:
+                index += 1
+            if index < len(s):
+                res += 1
+                index += 1
+        return res
+
+    @staticmethod
+    def is_perfect_square(num: int) -> bool:
+        if num == 1:
+            return True
+        l, r = 1, num // 2
+        while l <= r:
+            mid = (l + r) // 2
+            sq = mid * mid
+            if sq == num:
+                return True
+            if sq < num:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
+
+    @staticmethod
+    def search_insert(nums: list[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l
+
+    @staticmethod
+    def can_place_flowers(flowerbed: list[int], n: int) -> bool:
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i - 1] == 0 and flowerbed[i] == 0 and flowerbed[i + 1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+        return True if n <= 0 else False
+
+    @staticmethod
+    def find_max_consecutive_ones(nums: list[int]) -> int:
+        return len(max("".join([str(x) for x in nums]).split("0")))
+
+    @staticmethod
+    def find_length_of_LCIS(nums: list[int]) -> int:
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                dp[i] = dp[i - 1] + 1
+        return max(dp)
